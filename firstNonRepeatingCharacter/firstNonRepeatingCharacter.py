4d76d773f0e9a1fb4e19c58773a1ba42df08e922
Daniel Yoo <kwanukyoo@gmail.com>
2020-05-26 12:20:44 -0700

    firstNonRepeatingCharacter python -v

---
+++ firstNonRepeatingCharacter/firstNonRepeatingCharacter.py
@@ -0,0 +1,29 @@
+# This is first none repeating character problem sovled in python
+# question : find first non repeating character, return '_' if nothing found
+# Ex. abccdddefg return 'a'
+def firstNonRepeatingCharacter(word):    
+    dictionary = {}
+    # Fill in dictionary charac, numOfCharacter pair O(N)
+    for character in word:
+        if character in dictionary:
+            dictionary[character] = dictionary[character] + 1
+        else:
+            dictionary[character] = 1
+
+    # Iterate through word again O(N) but with lookup table which is dictinoary O(1)        
+    for character in word:        
+        if dictionary.get(character) == 1:    
+            print(character)        
+            return character
+                
+    print('_')
+    return '_'
+        
+
+# Test        
+firstNonRepeatingCharacter("aabcdd")
+firstNonRepeatingCharacter("aabbcdef")
+firstNonRepeatingCharacter("abccdd")
+firstNonRepeatingCharacter("aabbcc")
+
+
